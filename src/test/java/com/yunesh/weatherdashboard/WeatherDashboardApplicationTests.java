76cba257182ba6db9b0bfd558aab1cdedbf2570b
Yunesh Timsina <yuneshtimsina@gmail.com>
2025-09-06 18:12:08 +0545

    chore(init): project skeleton with Spring Boot + MongoDB

---
+++ src/test/java/com/yunesh/weatherdashboard/WeatherDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yunesh.weatherdashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherDashboardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
